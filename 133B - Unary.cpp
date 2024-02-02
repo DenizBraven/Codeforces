f5e628595c3e3abb4bdd4ea6f6d8d00b2cefcda2
Deniz Braven <123462896+DenizBraven@users.noreply.github.com>
2024-02-02 23:50:31 +0530

    Add files via upload

---
+++ 133B - Unary.cpp
@@ -0,0 +1,54 @@
+
+#include <cstdio>
+
+int main()
+{
+    char p[101];
+    scanf("%s", p);
+    int size(0);
+    for (size_t i = 0; p[i] != 0; ++i)
+    {
+        size *= 16;
+        switch (p[i])
+        {
+        case '>':
+            size += 8;
+            break;
+
+        case '<':
+            size += 9;
+            break;
+
+        case '+':
+            size += 10;
+            break;
+
+        case '-':
+            size += 11;
+            break;
+
+        case '.':
+            size += 12;
+            break;
+
+        case ',':
+            size += 13;
+            break;
+
+        case '[':
+            size += 14;
+            break;
+
+        case ']':
+            size += 15;
+            break;
+
+        default:
+            break;
+        }
+
+        size %= 1000003;
+    }
+    printf("%d\n", size);
+    return 0;
+}
